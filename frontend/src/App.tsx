8ffaccc7363d8f244d8e6f041567c07c8a6a916d
leya <playzet.srsly@mail.ru>
2025-06-19 21:36:21 +0300

    Frontend project created & shadcn set up

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className='flex h-screen items-center justify-center bg-zinc-900'>
+			<h1 className='text-3xl font-bold text-white underline'>
+				Tailwind CSS работает!
+			</h1>
+		</div>
+	)
+}
+
+export default App
