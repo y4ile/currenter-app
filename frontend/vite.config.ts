32f009335f277a12b0c710a2d4c6656778e0b3ff
leya <playzet.srsly@mail.ru>
2025-06-20 13:23:27 +0300

    Font updated & fixed modules

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [tailwindcss(), react()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
